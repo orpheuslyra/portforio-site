d77cad9030bf63ea6f0ff842bdac1e9f9a068c8f
MitsuoKoizumi <m_koizumi@yumemi.co.jp>
2025-01-13 23:04:56 +0900

    初期設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: "",
+  build: {
+    assetsPrefix: ".",
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@": new URL("./src", import.meta.url).pathname,
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+                @import "@/styles/reset.scss";
+                @import "@/styles/variants/_mixin.scss";
+                @import "@/styles/global.scss";
+                @import "@/styles/responsive.scss";`,
+        },
+      },
+    },
+  },
+});
